2c07fb7536fa0624e559cd7c1cbf7edc6211aee9
Zain <zain.khoso.dev@gmail.com>
2023-11-26 15:01:28 +0500

    Developer Security update

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,17 +1,21 @@
 "use strict";
 
-// Initializing EmailJs.
-emailjs.init("0-AyLKyEwxZwIq1Gi");
+import emailjs from "@emailjs/browser";
 
 // DOM Selections.
 const elem_form = document.querySelector(".contact-section");
 
-// Functions.
-const sendEmail = function (event) {
-    event.preventDefault();
+// // Functions.
+// const sendEmail = function (event) {
+//     event.preventDefault();
 
-    emailjs.sendForm("contact_service", "contact_form", this);
-};
+//     emailjs.sendForm(
+//         "contact_service",
+//         "contact_form",
+//         this,
+//         "0-AyLKyEwxZwIq1Gi"
+//     );
+// };
 
 // Event Listeners.
 elem_form.addEventListener("submit", sendEmail);
